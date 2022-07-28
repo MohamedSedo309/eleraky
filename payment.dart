2b049754a59440625aadbd565282994d4c88fb77
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2022-07-28 17:46:36 +0200

    Add files via upload

---
+++ payment.dart
@@ -0,0 +1,12 @@
+class Payment {
+  int payment;
+  String date;
+  String reciever;
+  String payment_ID;
+
+  Payment(
+      {required this.payment,
+      required this.date,
+      required this.reciever,
+      required this.payment_ID});
+}
